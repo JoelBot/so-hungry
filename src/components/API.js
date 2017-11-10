3a01818cf9c44c071f097ecb4811d92d82bf0c22
Rae-Lyn Morikawa <rrmorikawa@gmail.com>
2017-11-10 11:08:13 -0800

    userform and other things

--- src/components/API.js
+++ src/components/API.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../css/App.css';
+import UserForm from './UserForm';
+
+function VisibleForm (props){
+    if(!props.showForm){
+        return null;
+    }
+    return(
+        <UserForm/>
+    )
+}
 
 class API extends Component {
     constructor(props) {
         super(props)
-        this.fetchAPI = this.fetchAPI.bind(this)
-        this.updateState = this.updateState.bind(this)
+        // this.fetchAPI = this.fetchAPI.bind(this)
+        // this.updateState = this.updateState.bind(this) // you do not need this to be bound because you are not passing it anywhere
         this.state = {
-            name: '',
-            model: ''
+            name: ''
         }
+        this.state = {showForm: false}
+        this.handleToggleForm = this.handleToggleForm.bind(this);
     }
 
-    fetchAPI() {
-    //    https://swapi.co/api/starships/9/
+    // componentWillMount() {
+    //     const formData = new FormData();
+    //     formData.append('client_id', 'FW1RHqeh1ELGz9m5U4UBCA');
+    //     formData.append('client_secret', 'k3JsolEiChW9Ua1eOI67sjJMU9Ad6cKkgna8hozWZFtY8jrQzxINHCvyntq2LvHm');
+    //     formData.append('grant_type', 'client_credentials');
+    //     fetch('https://api.yelp.com/oauth2/token', {
+    //         method: 'POST',
+    //         body: formData,
+    //         mode: 'cors',
+    //     })
+    //         .then((response) => response.json())
+    //         .then((response) => this.setState({ auth: response }));
+    // }
+    // you can use arrow functions to autobind: stage 2 proposal
+    // fetchAPI = () => {
+    // //    https://swapi.co/api/starships/9/
+    //
+    //     // when using no-cors, only certain headers are allowed. authorization is not one of them, thus 401
+    //     var request = {
+    //         method: 'GET',
+    //         headers: new Headers({
+    //             'Authorization': 'Bearer ' + this.state.auth.auth_token,
+    //
+    //             //'Access-Control-Allow-Origin': '*'
+    //         }),
+    //         mode: 'cors'
+    //     };
 
-        fetch('https://swapi.co/api/starships/9/')
-            .then(response => response.json())
-            .then(response => this.updateState(response))
+    //     fetch('https://api.yelp.com/v3/businesses/ferry-noodle-house-seattle', request)
+    //         .then(response => response.json())
+    //         .then(response => this.updateState(response))
+    //
+    // }
 
-    }
-
+    // rename to distinguish between componenet state and this response
     updateState(response) {
         this.setState({
-            name: response.name,
-            model: response.model
+            name: response.name
         })
     }
+
+
+
+    handleToggleForm(){
+        this.setState(prevState =>({
+            showForm: !prevState.showForm
+        }));
+    }
+
 
     render() {
         return (
@@ -35,13 +80,15 @@ class API extends Component {
                 <div className="col-sm-12">
                     <div className="col-sm-2">
                         <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
+                            <iframe src="https://giphy.com/embed/jlEkt1QTAZJXa" width="480" height="206" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
                         </header>
                     </div>
-                    <div className="col-sm-10">
-                        <button className="IDKButton" onClick={this.fetchAPI}>CLICK</button>
-                        <p>{this.state.name}</p>
-                        <p>{this.state.model}</p>
+
+                    <div className="col-sm-10 padding">
+                        <button className="IDKButton" onClick={this.handleToggleForm}>Start new search</button>
+                    </div>
+                    <div>
+                        <VisibleForm showForm={this.state.showForm} />
                     </div>
                 </div>
             </div>
