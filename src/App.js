ce06c9c2b1ffa7d6eb4bd33dec363bda6e909062
Joel Lonneker <joel.lonneker@gmail.com>
2017-11-14 15:13:50 -0800

    removed xml from Intellij, refactored file names and more sass.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './css/_api.scss';
+import './css/_app.scss';
 
 class App extends Component {
   render() {
---
+++ src/components/LeftCol.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LeftCol extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className={"col-sm-2 left-col"}>
+                Testing Two
+            </div>
+        )
+    }
+}
+
+export default LeftCol;
